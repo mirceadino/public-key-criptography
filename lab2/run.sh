3607970b8577ff30235c0b9d4dfe8f31c8a87499
mirko <mirceadino97@gmail.com>
2017-11-07 18:55:08 +0200

    Test methods on various inputs

---
+++ lab2/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+make generator
+make main
+
+timeout=2
+
+for i in 2 4 8 16 32 64 128 256 512 1024; do
+  ./generator "$i" > tmp 2> /dev/null
+  echo "Numbers of $i digits"
+  cat tmp
+  echo "GCD"
+  ./main 1 < tmp 2> /dev/null
+  echo "Results"
+  gtimeout "$timeout" ./main 1 < tmp > /dev/null
+  gtimeout "$timeout" ./main 2 < tmp > /dev/null
+  # gtimeout "$timeout" ./main 3 < tmp > /dev/null
+  gtimeout "$timeout" ./main 4 < tmp > /dev/null
+  echo "---"
+done;
+
+make clean
+rm tmp
