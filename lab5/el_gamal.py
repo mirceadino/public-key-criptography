9b0d976889842b655c6be8dfdb9da0e6490d384d
Al3ks1002 <al3ks1002@gmail.com>
2018-01-18 18:16:12 +0200

    added big prime and log power

--- lab5/el_gamal.py
+++ lab5/el_gamal.py
@@ -28,24 +28,28 @@ def decode(message):
         else:
             plaintext += chr(ord('a') + r - 2)
         message //= BASE
-    return plaintext
+    return plaintext[::-1]
 
-def phi(x):
-    num_coprimes = 0
-    for i in range(1, x):
-        if gcd(i, x) == 1:
-            num_coprimes +=1
-    return num_coprimes
+def invmod(x, p):
+    return lgpow(x, p - 2, p);
 
-def invmod(x, n):
-    return x ** (phi(n) - 1) % n
+def lgpow(a, b, mod):
+    r = 1
+    i = 1
+    while i <= b:
+        if i & b:
+            r = r * a % mod
+        a = a * a % mod
+        i *= 2
+    return r
 
 class ElGamal:
     def __init__(self):
-        self._p = 104729
-        self._g = 12
+        f = open("prime.txt", 'r')
+        self._p = int(f.read(), 16)
+        self._g = 2
         self._a = random.randint(1, self._p - 2)
-        self._ga = (self._g ** self._a) % self._p
+        self._ga = lgpow(self._g, self._a, self._p)
 
     def get_public_key(self):
         return [self._p, self._g, self._ga]
@@ -57,11 +61,11 @@ class ElGamal:
         message = encode(plaintext)
         print(message)
         k = random.randint(1, self._p - 2)
-        alpha = (self._g ** k) % self._p
-        beta = message * ((self._ga) ** k) % self._p
+        alpha = lgpow(self._g, k, self._p)
+        beta = message * lgpow(self._ga, k, self._p)
         return alpha, beta
 
     def decrypt(self, alpha, beta):
-        message = (invmod(alpha, self._p) ** self._a) % self._p * beta % self._p
+        message = lgpow(invmod(alpha, self._p), self._a, self._p) * beta % self._p
         print(message)
         return decode(message)
