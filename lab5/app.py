c9b2ad9ee8e5ea5f3eee110411144ea67c715c22
Al3ks1002 <al3ks1002@gmail.com>
2018-01-18 17:35:59 +0200

    initial version - does not work

---
+++ lab5/app.py
@@ -0,0 +1,112 @@
+from flask import Flask, request, redirect, render_template, url_for, jsonify
+from el_gamal import ElGamal
+from math import gcd
+
+app = Flask(__name__)
+app.jinja_env.auto_reload = True
+
+
+def contains_only_characters_from(text, alphabet):
+    for x in text:
+        if x not in alphabet:
+            return False
+    return True
+
+
+def is_good_alphabet(alphabet):
+    d = {}
+    for x in alphabet:
+        if x not in d:
+            d[x] = 0
+        d[x] += 1
+    for x in d.values():
+        if x > 1:
+            return False
+    basic_alphabet = "_abcdefghijklmnopqrstuvwxyz"
+    return contains_only_characters_from(alphabet, basic_alphabet)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+el_gamal = ElGamal()
+plaintext = "encode_this_text"
+public_key = el_gamal.get_public_key()
+private_key = el_gamal.get_private_key()
+alpha = 1
+beta = 1
+
+@app.route('/encrypt', methods=['GET', 'POST'])
+def encrypt():
+    global el_gamal
+    global plaintext
+    global public_key
+    global private_key
+    global alpha
+    global beta
+    warning = ""
+    if request.method == 'POST':
+        plaintext = request.form['plaintext']
+        p = request.form['p']
+        g = request.form['g']
+        ga = request.form['ga']
+        private_key = request.form['private_key']
+        try: 
+            p = int(p)
+            g = int(g)
+            ga = int(ga)
+            public_key = [p, g, ga]
+            private_key = int(private_key)
+        except:
+            warning = "Invalid public key."
+        alpha, beta = el_gamal.encrypt(plaintext)
+    return render_template('encrypt.html', \
+            plaintext=plaintext, \
+            p=public_key[0], \
+            g=public_key[1], \
+            ga=public_key[2], \
+            private_key=private_key, \
+            alpha=alpha, \
+            beta=beta, \
+            warning=warning)
+
+
+@app.route('/decrypt', methods=['GET', 'POST'])
+def decrypt():
+    global el_gamal
+    global plaintext
+    global public_key
+    global private_key
+    global alpha
+    global beta
+    warning = ""
+    if request.method == 'POST':
+        plaintext = request.form['plaintext']
+        p = request.form['p']
+        g = request.form['g']
+        ga = request.form['ga']
+        alpha = request.form['alpha']
+        beta = request.form['beta']
+        try: 
+            p = int(p)
+            g = int(g)
+            ga = int(ga)
+            public_key = [p, g, ga]
+            private_key = int(private_key)
+            alpha = int(alpha)
+            beta = int(beta)
+        except:
+            warning = "Invalid public key."
+        plaintext = el_gamal.decrypt(alpha, beta)
+    return render_template('decrypt.html', \
+            plaintext=plaintext, \
+            p=public_key[0], \
+            g=public_key[1], \
+            ga=public_key[2], \
+            private_key=private_key, \
+            alpha=alpha, \
+            beta=beta, \
+            warning=warning)
+
---
+++ lab5/el_gamal.py
@@ -0,0 +1,61 @@
+import random
+import math
+
+BASE = 28
+
+def encode(plaintext):
+    message = 0
+    for c in plaintext:
+        if c == '_':
+            char_value = 1
+        else:
+            char_value = ord(c) - ord('a') + 2
+        message = message * BASE + char_value
+    return message
+
+def decode(message):
+    plaintext = ""
+    while message > 0:
+        r = message % BASE
+        if r == 1:
+            plaintext += '_'
+        else:
+            plaintext += chr(ord('a') + r - 2)
+        message //= BASE
+    return plaintext
+
+def phi(x):
+    num_coprimes = 0
+    for i in range(1, x):
+        if math.gcd(i, x) == 1:
+            num_coprimes +=1
+    return num_coprimes
+
+def invmod(x, n):
+    return x ** (phi(n) - 1) % n
+
+class ElGamal:
+    def __init__(self):
+        self._p = 104729
+        self._g = 12
+        self._a = random.randint(1, self._p - 2)
+        self._ga = (self._g ** self._a) % self._p
+
+    def get_public_key(self):
+        return [self._p, self._g, self._ga]
+
+    def get_private_key(self):
+        return self._a
+
+    def encrypt(self, plaintext):
+        message = encode(plaintext)
+        print(message)
+        k = random.randint(1, self._p - 2)
+        alpha = (self._g ** k) % self._p
+        beta = message * ((self._ga) ** k) % self._p
+        return alpha, beta
+
+    def decrypt(self, alpha, beta):
+        message = invmod(alpha, self._a) * beta % self._p
+        print(message)
+        return decode(message)
